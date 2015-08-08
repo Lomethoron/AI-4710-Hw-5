37edfda50b8a074f218881382acbd0da3e237ebd
Will Dixon <wdixon23192@gmail.com>
2015-08-08 13:54:50 -0400

    initial commit

---
+++ wwd9d_rjh8h_Classifier.java
@@ -0,0 +1,27 @@
+
+
+public class wwd9d_rjh8h_Classifier extends Classifier {
+
+	public wwd9d_rjh8h_Classifier() {
+		super("");
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void train(String trainingDataFilpath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makePredictions(String testDataFilepath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
